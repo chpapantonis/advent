f1345cf97d5851fb810c0a811ff746c1a7d4e66f
Christos Papantonis <christos.papantonis@skroutz.gr>
2021-12-13 11:49:10 +0200

    3rd day, 2nd half

--- 2021/day3/day3.playground/Contents.swift
+++ 2021/day3/day3.playground/Contents.swift
@@ -17,8 +17,6 @@ let file = "bits"
 
 let contents = load(file: file)
 
-var twosComplement:[Int16] = []
-
 guard let bits = contents?.split(separator: "\n").compactMap({Array($0)}) else {
     fatalError()
 }
@@ -28,6 +26,8 @@ let arraySize = bits.count
 
 var gammaRate: [Character] = []
 var epsilonRate: [Character] = []
+
+// 1rst solution
 
 for bitIdx in 0..<stringSize {
     var ones = 0
@@ -48,6 +48,56 @@ for bitIdx in 0..<stringSize {
 
 print(gammaRate)
 print(epsilonRate)
-let multiply = Int(String(gammaRate), radix: 2)! * Int(String(epsilonRate), radix: 2)!
+var multiply = Int(String(gammaRate), radix: 2)! * Int(String(epsilonRate), radix: 2)!
 print(multiply)
 
+
+// 2nd solution
+
+var oxygen = bits
+var carbonDioxide = bits
+
+func makeArray(array: [[Character]], index: Int, bit: Character) -> [[Character]] {
+    var temp: [[Character]] = []
+    array.forEach {
+        if $0[index] == bit { temp.append($0) }
+    }
+    return temp
+}
+
+for bitIdx in 0..<stringSize {
+    var ones = 0
+    var zeros = 0
+    for stringIdx in 0..<oxygen.count {
+        if oxygen[stringIdx][bitIdx] == "1" {
+            ones += 1
+        } else {
+            zeros += 1
+        }
+    }
+    let mostFreq: Character = ones >= zeros ? "1" : "0"
+    if oxygen.count > 1 {
+        oxygen = makeArray(array: oxygen, index: bitIdx, bit: mostFreq)
+    }
+}
+print(oxygen)
+
+for bitIdx in 0..<stringSize {
+    var ones = 0
+    var zeros = 0
+    for stringIdx in 0..<carbonDioxide.count {
+        if carbonDioxide[stringIdx][bitIdx] == "1" {
+            ones += 1
+        } else {
+            zeros += 1
+        }
+    }
+    let lessFreq: Character = ones < zeros ? "1" : "0"
+    if carbonDioxide.count > 1 {
+        carbonDioxide = makeArray(array: carbonDioxide, index: bitIdx, bit: lessFreq)
+    }
+}
+print(carbonDioxide)
+
+multiply = Int(String(oxygen[0]), radix: 2)! * Int(String(carbonDioxide[0]), radix: 2)!
+print(multiply)
